89fc1b3cb3abd62f2ed7510fe635a91ce9c915ba
Daedra <foglzerika@gmail.com>
2020-04-24 23:44:40 -0500

    fix | comment code to access-controll

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ app.use(morgan('dev'));
 
 
 
+/*
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", '*');
     res.header("Access-Control-Allow-Credentials", true);
@@ -38,6 +39,7 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
     next();
 });
+*/
 
 
 

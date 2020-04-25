ae6b8b7f915931b7fe198417034a7c3cc0c546d7
Daedra <foglzerika@gmail.com>
2020-04-25 14:01:39 -0500

    feat | Adding .idea to gitignore file

--- routes/wallet-routes.js
+++ routes/wallet-routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const walletRoutes = express.Router();
-
+const five = require("johnny-five");
 const Wallet = require('./../models/wallet-model');
 
 walletRoutes.post('/wallets' , (req, res) =>{
@@ -39,5 +39,32 @@ walletRoutes.get('/wallets' , (req , res) =>{
 });
 
 
+walletRoutes.post('/price ', (req,res) =>{
+ const {name, price} = req.body;
+ console.log("Hola")
+ console.log(name,price);
+  const board = new five.Board()
 
+  board.on('ready', function() {
+   let  lcd = new five.LCD({
+    pins: [7, 8, 9, 10, 11, 12],
+    backlight: 6,
+    rows: 2,
+    cols: 16
+   });
+   lcd.clear().print("Hello");
+   lcd.cursor(1, 0);
+   lcd.clear().print(name);
+   lcd.clear();
+   lcd.cursor(1, 2);
+   lcd.clear().print(price);
+
+
+   this.repl.inject({
+    lcd: lcd
+   });
+
+  });
+
+})
 module.exports = walletRoutes;

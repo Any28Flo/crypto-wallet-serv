8c797ea7a48a243654ef6632b8ca9c0739bab51a
Daedra <foglzerika@gmail.com>
2020-04-24 23:28:20 -0500

    fix | fixing cors isues

--- app.js
+++ app.js
@@ -48,10 +48,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(cors({
-    credentials : true,
-    origin: ['http://localhost:3001']
-}));
+app.use(cors());
 
 
 
